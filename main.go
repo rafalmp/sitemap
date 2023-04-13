1d7d87f59c822d17f7e7903ca4397a2480125274
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-13 22:57:28 +0200

    Get top page contents

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
-	//
+	urlFlag := flag.String("url", "https://gophercises.com", "the url of the site to build the map for")
+	flag.Parse()
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
 }
