9b33d29f8ccc392b40da5269c360e08fb746aa2f
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-14 22:48:28 +0200

    Parse page and build links

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/rafalmp/sitemap
 
 go 1.20
+
+require github.com/rafalmp/link v0.0.0-20230412203442-d6eea88320e0
+
+require golang.org/x/net v0.9.0 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/rafalmp/link"
 )
 
 func main() {
@@ -16,5 +19,26 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	var hrefs []string
+	links, _ := link.Parse(resp.Body)
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
